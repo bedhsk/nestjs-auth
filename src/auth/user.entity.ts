3751388453c1dab70012c42a34282fdfa5c0118c
bedhsk <brianbatz03@gmail.com>
2026-02-02 13:40:48 -0600

    feat(auth): user enity created

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,4 +1,28 @@
-import { Entity } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
-export class User {}
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 20 })
+  name: string;
+
+  @Column({ length: 100 })
+  email: string;
+
+  @Column({ type: 'boolean', default: false })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}

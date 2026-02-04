7ddf32f37f3ad96ceb900fcea2fab65980178452
bedhsk <brianbatz03@gmail.com>
2026-02-03 18:40:37 -0600

    feat: add validation for user creation DTO and enable validation pipes

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,13 +1,23 @@
-import { IsEmail, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+  Length,
+} from 'class-validator';
 
 export class CreateUserDto {
+  @IsNotEmpty()
   @IsString()
+  @Length(2, 20)
   name: string;
 
   @IsString()
   @IsEmail()
   email: string;
 
+  @IsNotEmpty()
   @IsString()
+  // @IsStrongPassword()
   password: string;
 }
